b212c015b33617b610b65aa2c715d67c71e23934
Jeovan Mills <164845905+jmacker101@users.noreply.github.com>
2025-08-09 22:23:57 -0400

    Add files via upload
    starting files for making new website for myself

---
+++ website_portfolio/src/constants/index.js
@@ -0,0 +1,82 @@
+// all imports that need to be changed to project i am working on and completed.
+import project1 from "../assets/projects/FitMon.png";
+import project2 from "../assets/projects/GottaCodeEmAll.png";
+import project3 from "../assets/projects/portfolio.png";
+import project4 from "../assets/projects/Root2Roast.png";
+
+// what i aim to be later for myself.
+export const HERO_CONTENT = `I am  full stack developer whom is passionate and hunger for learning how to craft robust and scalable web applications. Who wants hands-on experience, I want to honed my skills in front-end technologies like React and Next.js, as well as back-end technologies like Node.js, MySQL, PostgreSQL, and MongoDB. My goal is to leverage my expertise to create innovative solutions that drive business growth and deliver exceptional user experiences.`;
+
+// all about me and what i have done and what i want.
+export const ABOUT_TEXT = `I want to dedicate my abilities which can be versatile to learn and become a full stack developer with passion for creating efficient and user-friendly web applications. With professional experience, I aim to work with a variety of technologies, including React, Next.js, Node.js, MySQL, PostgreSQL, and MongoDB and more given the chance to in the future. My journey in web development began with deep curiosity for how things work, and it has evolved into a career that i want for myself. Where I continuously strive to learn and adapt to new challenges. I thrive in collaborative environments and enjoy solving complex problems to deliver high-quality solutions. Outside of coding, I enjoy staying active, exploring new technologies, and contributing to open-source projects with great communication.`;
+
+export const EXPERIENCES = [
+  {
+    // exclude the experiences from the portfolio 
+    year: "2023 - Present",
+    role: "Senior Full Stack Developer",
+    company: "Google Inc.",
+    description: `Led a team in developing and maintaining web applications using JavaScript, React.js, and Node.js. Implemented RESTful APIs and integrated with MongoDB databases. Collaborated with stakeholders to define project requirements and timelines.`,
+    technologies: ["Javascript", "React.js", "Next.js", "mongoDB"],
+  },
+  {
+    year: "2022 - 2023",
+    role: "Frontend Developer",
+    company: "Adobe",
+    description: `Designed and developed user interfaces for web applications using Next.js and React. Worked closely with backend developers to integrate frontend components with Node.js APIs. Implemented responsive designs and optimized frontend performance.`,
+    technologies: ["HTML", "CSS", "Vue.js", "mySQL"],
+  },
+  {
+    year: "2021 - 2022",
+    role: "Full Stack Developer",
+    company: "Facebook",
+    description: `Developed and maintained web applications using JavaScript, React.js, and Node.js. Designed and implemented RESTful APIs for data communication. Collaborated with cross-functional teams to deliver high-quality software products on schedule.`,
+    technologies: ["Python", "Svelte", "Three.js", "Postgres"],
+  },
+  {
+    year: "2020 - 2021",
+    role: "Software Engineer",
+    company: "Paypal",
+    description: `Contributed to the development of web applications using JavaScript, React.js, and Node.js. Managed databases and implemented data storage solutions using MongoDB. Worked closely with product managers to prioritize features and enhancements.`,
+    technologies: ["Ruby", "Rails", "PHP", "Sqlite"],
+  },
+]; 
+
+export const PROJECTS = [
+  {
+    // Change all projects that i have done in class to projects i have done now.
+    title: "FitMon",
+    image: project1,
+    description:
+     "An application for managing workouts by choosing certain body parts and tracking your progress that you choose.while also playing a broswer game for pokemon",
+    technologies: ["HTML", "CSS", "JavaScript", "TypeScript"],
+  },
+  {
+    title: "GottaCodeEmAll",
+    image: project2,
+    description:
+    "A functional application is to build a Pokémon site that allows users to discover the evolution levels and methods for each Pokémon.while also aiming to provide a tracker for finding out what level or how your Pokemon evolves, as well as for building a team you like.",
+    technologies: ["HTML", "CSS", "JavaScript", "Handlebars.js"],
+  },
+  {
+    
+    title: "Portfolio Website",
+    image: project3,
+    description:
+      "A personal portfolio website showcasing projects, skills, and contact information.",
+    technologies: ["HTML", "CSS", "React", "JavaScript", "Tailwind"],
+  },
+  {
+    title: "Root2Roast",
+    image: project4,
+    description:
+      "Provides a personalized profile, communities & groups, inspiration & discovery, and an activity feed.",
+    technologies: ["HTML", "CSS", "TypeScript", "Express", "mySQL", "Postgress", "JavaScript"],
+  },
+];
+//  no address  only phone number and email they can reach me at.
+export const CONTACT = {
+  address: "",
+  phoneNo: "352-445-7271",
+  email: "jm470t@gmail.com",
+};
